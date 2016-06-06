8741e1e3afb6cb944a5e8d83ce0e53fd80dd2213
bianyuzhang <bianyuzhang@ezjie.cn>
2016-06-06 11:44:42 +0800

    0606

--- GitManager/app/src/main/java/com/example/ezj213/gitmanager/MainActivity.java
+++ GitManager/app/src/main/java/com/example/ezj213/gitmanager/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        asdasdasadasdasdasdsa
+
+        System.out.println("v1.0.0");
     }
 }
